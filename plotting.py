7c30694bd93ab9ea833001d9b0d6ed1ef7ce22ad
George Gayton <ggayton95@gmail.com>
2022-04-27 12:34:49 +0100

    plot ellipse update

--- plotting.py
+++ plotting.py
@@ -14,21 +14,6 @@ def plot_array(array, fig_num = [], close = True):
     matplotlib.pyplot.imshow(array)
     matplotlib.pyplot.show()
 
-def plot_scatter(X, Y, indexing = 'ij', fig_num = [], close = True):
-    
-    if close:
-        matplotlib.pyplot.close(fig_num)
-    
-    if fig_num:
-        matplotlib.pyplot.figure(fig_num)
-    
-    if indexing == 'ij':
-        matplotlib.pyplot.scatter(X,Y,s=1, c='red')
-    if indexing == 'xy':
-        matplotlib.pyplot.scatter(Y,X,s=1, c='red')
-        
-    matplotlib.pyplot.show()
-
 def bivariate_normal_distribution(x,y,meanX,meanY,stdX,stdY,corr):
     
         dist = (1/( stdX*stdY*2*np.pi*(1-corr**2)**0.5)) *\
@@ -225,3 +210,71 @@ def plotErrorEllipsoid(ax, point, V, lineArgs = 'r'):
     plt.show()    
     
     return 
+
+def plotEllipseFromMat(self,x0,y0,a,b,T,L=20,N=100):
+    
+    x,y = np.meshgrid(np.linspace(-L,L,N), np.linspace(-L,L,N), indexing = 'ij')
+    
+    vec = np.empty((3,N**2))
+    vec[0,:] = x.flatten() + x0
+    vec[1,:] = y.flatten() + y0
+    vec[2,:] = 1
+    
+    M = self.defineMatrix(self.defineCoeffs(x0,y0,a,b,T))
+
+    test = np.sum(vec * (M @ vec), axis=0)
+    test = test.reshape(N,N)
+
+    plt.imshow(np.abs(test<0).T, extent = [L+x0,-L+x0,-L+y0,L+y0], cmap = 'gray')
+    
+def plotEllipseContour(x0,y0,a,b,T,L=20,N=100):
+    
+    x,y = np.meshgrid(np.linspace(x0-L,x0+L,N), np.linspace(y0-L,y0+L,N), indexing = 'ij')
+    
+    vec = np.empty((3,N**2))
+    vec[0,:] = x.flatten()
+    vec[1,:] = y.flatten()
+    vec[2,:] = 1
+    
+    M = defineMatrix(defineCoeffs(x0,y0,a,b,T))
+
+    test = np.sum(vec * (M @ vec), axis=0)
+    test = test.reshape(N,N)
+
+    plt.contour(x,y,test, [0], colors = 'red') 
+
+def defineMatrix(coeffs):
+
+    A,B,C,D,E,F = coeffs    
+
+    M = np.empty((3,3))
+    M[0,0] = A
+    M[0,1] = B/2
+    M[0,2] = D/2
+    M[1,0] = B/2
+    M[1,1] = C
+    M[1,2] = E/2
+    M[2,0] = D/2
+    M[2,1] = E/2
+    M[2,2] = F
+
+    return M
+
+def defineCoeffs(x,y,a,b,T):
+    A = a**2*np.sin(T)**2 + b**2*np.cos(T)**2
+    B = 2*(b**2 - a**2)*np.sin(T)*np.cos(T)
+    C = a**2*np.cos(T)**2 + b**2*np.sin(T)**2
+    D = -2*A*x - B*y
+    E = -B*x - 2*C*y
+    F = A*x**2 + B*x*y + C*y**2 - a**2*b**2
+        
+    return A,B,C,D,E,F
+
+def plotEllipseLine(x0,y0,a,b,T,N=100,c=''):
+    
+    theta = np.linspace(0,2*np.pi,N)
+    
+    x = a*np.cos(theta - T) + x0
+    y = b*np.sin(theta - T) + y0
+    
+    plt.plot(x,y,c)
